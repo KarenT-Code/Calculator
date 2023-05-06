60029aa7a4225c7b26cf382f8c1d419287845ab1
KarenT-Code <130744009+KarenT-Code@users.noreply.github.com>
2023-05-06 12:32:32 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,105 @@
+const display = document.querySelector(".display");
+const numbers = document.querySelectorAll(".number");
+const operator = document.querySelectorAll(".operator");
+const equal = document.querySelector(".equal");
+const clearAll = document.querySelector(".clearAll");
+const clearLast = document.querySelector(".clearLast");
+let displayNumber= "";
+let result = null;
+let lastOperator = "";
+let haveDot = false;
+let tempResult = "";
+
+numbers.forEach((number) => {
+  number.addEventListener("click", function(e) {
+    if (e.target.innerText === "." && !haveDot) {
+      haveDot = true;
+      if(!displayNumber){
+        displayNumber = 0; 
+      }
+    } else if (e.target.innerText === "." && haveDot) {
+      return
+    }
+
+    if (tempResult == "") {
+        displayNumber += e.target.innerText;
+        display.innerText = displayNumber;
+    } 
+    else{
+        displayNumber += e.target.innerText;
+        display.innerText = tempResult + displayNumber;
+    }
+  });
+});
+
+operator.forEach((operator) => {
+operator.addEventListener("click", (e) => {
+  if (!displayNumber || displayNumber <= 0) return; 
+  haveDot = false;
+
+  const operatorName = e.target.innerText;
+  
+  if (displayNumber && lastOperator) {
+    operation();
+    tempResult = `${result}${operatorName}`
+  } else {
+    result = parseFloat(displayNumber);
+    tempResult = displayNumber += operatorName;
+    display.innerHTML = tempResult;
+  }
+  
+  lastOperator = operatorName;
+  displayNumber = '';
+});
+});
+
+
+function operation() {
+  if (lastOperator === "x") {
+    result = parseFloat(result) * parseFloat(displayNumber);
+    display.innerHTML = Math.round(result);
+  } else if (lastOperator === "+") {
+    result = parseFloat(result) + parseFloat(displayNumber);
+    display.innerHTML = Math.round(result);
+  } else if (lastOperator === "-") {
+    result = parseFloat(result) - parseFloat(displayNumber);
+    display.innerHTML = Math.round(result);
+  } else if (lastOperator === "/") {
+    result = parseFloat(result) / parseFloat(displayNumber);
+    if (parseFloat(result) === 0 || parseFloat(displayNumber) === 0) {
+      display.innerHTML = "clear screen ";
+      clear();
+      return;
+    }
+    display.innerHTML = Math.round(result);
+  } else if (lastOperator === "%") {
+    result = parseFloat(result) % parseFloat(displayNumber);
+    display.innerHTML = Math.round(result);
+  }
+}
+
+equal.addEventListener("click", () => {
+  if (!displayNumber || !result) return;
+  displayNumber = operation().toString();
+  display.innerHTML = displayNumber;
+  lastOperator = "";
+});
+
+clearAll.addEventListener("click", () => {
+  clear();
+});
+
+function clear() {
+  display.innerText = "0";
+  displayNumber= "";
+  result = null;
+  lastOperator = "";
+  haveDot = false;
+  tempResult = "";
+}
+
+clearLast.addEventListener("click", () => {
+  let len = displayNumber.length;
+  displayNumber = displayNumber.slice(0,len-1);
+  display.innerText = displayNumber;
+});
